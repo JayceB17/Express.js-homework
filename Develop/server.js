e50ed4cdd241c7341a7573ae1c1f815c05b8e093
JayceB17 <jayceb.ham17@yahoo.com>
2023-07-20 18:14:08 -0600

    made small change to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -12,12 +12,12 @@ app.use(express.static('public'));
 const database = require('./db/db.json');
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
-});
+}); 
 
 app.get('/api/notes', (req, res) => {
   res.json(database.slice(1));
