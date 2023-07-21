9fde3cf0393a192a62227c274b8c72fc8180145c
JayceB17 <jayceb.ham17@yahoo.com>
2023-07-21 14:46:47 -0600

    I redid my server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,74 +1,44 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+const { v4: uuidv4 } = require('uuid');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const database = require('./db/db.json');
+//const database = require('./db/db.json');
+
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
+//* is a wildcard that matches any string
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
-}); 
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 app.get('/api/notes', (req, res) => {
-  res.json(database.slice(1));
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+  res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-  const newNote = newNoteDb(req.body, database);
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf8'));
+  notes.push(newNote);
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notes));
   res.json(newNote);
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-  removeNote(req.params.id, database);
-  res.json(true);
-});
-
-function newNoteDb(body, notesArray) {
-  const newNote = body;
-  if (!Array.isArray(notesArray))
-    notesArray = [];
-
-  if (notesArray.length === 0)
-    notesArray.push(0);
-
-  body.id = notesArray[0];
-  notesArray[0]++;
-
-  notesArray.push(newNote);
-  fs.writeFileSync(
-    path.join(__dirname, './db/db.json'),
-    JSON.stringify(notesArray, null, 2)
-  );
-  return newNote;
-}
-
-function removeNote(id, notesArray) {
-  for (let i = 0; i < notesArray.length; i++) {
-    let note = notesArray[i];
-
-    if (note.id == id) {
-      notesArray.splice(i, 1);
-      fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify(notesArray, null, 2)
-      );
-
-      break;
-    }
-  }
-}
-
-// Start the server
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log(`App listening on http://localhost:${PORT}`);
 });
